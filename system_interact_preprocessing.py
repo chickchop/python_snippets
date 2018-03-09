420fcbef3e101adc71de7c10162bb7e2dda6de36
chickchop <kohop8888@gmail.com>
2018-03-09 16:40:51 +0900

    update
    새로운 함수 추가

--- system_interact_preprocessing.py
+++ system_interact_preprocessing.py
@@ -1,5 +1,5 @@
 '''
-프로 디스커버리를 적용할 때 대용량 log file을 조작하기 위한 모듈
+시스템 간 분석을 위해 프로 디스커버리를 적용할 때 대용량 log file을 조작하기 위한 모듈
 
 Created on 2018.02.14
 
@@ -12,8 +12,16 @@ import os
 import time
 import csv
 from operator import itemgetter,attrgetter
+import datetime
 
-def remove_duplicate_activity_by_pandas(in_file_path, out_file_path, encoding='utf-8', case_id='CaseID', timestamp='Timestamp', activity='Activity'):
+def progressBar(value, endvalue, bar_length=20):
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+    sys.stdout.write("\rPercent: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
+
+def remove_duplicate_activity_by_pandas(in_file_path, case_id, timestamp, activity, out_file_path, encoding='utf-8'):
     '''
     케이스당 연속되는 엑티비티를 중복으로 보고 삭제하는 함수. pandas를 이용
     
@@ -21,14 +29,14 @@ def remove_duplicate_activity_by_pandas(in_file_path, out_file_path, encoding='u
     -------------------------------
     in_file_path : string / header와 reader의 형태로 존재하는 raw log file형태 / raw log file 경로
     out_file_path : string / header와 reader의 형태로 존재하는 raw log file형태 / 새로 작성될 raw log file 경로
-    case_id_idx : int /   / case id 열 위치
-    activity_idx : int /    / activity 열 위치
-    timestamp_idx : int /    / timestamp 열 위치
+    case_id : string /   / case id 열 위치
+    activity : string /    / activity 열 위치
+    timestamp : string /    / timestamp 열 위치
     encoding : stirng /    / raw log file을 읽어드릴 때 encoding 방법
     '''
     
     df = pd.read_csv(in_file_path, encoding= encoding)
-    df.sort_values(by=[case_id,timestamp]).drop_duplicates(keep=False)
+    df.sort_values(by=[case_id,timestamp], inplace= True)
     k = 1
     while k < len(df):
         if df[case_id].iloc[k] == df[case_id].iloc[k-1]:
@@ -40,14 +48,7 @@ def remove_duplicate_activity_by_pandas(in_file_path, out_file_path, encoding='u
                     k = k - 1
         k = k + 1
 
-    df.to_csv(out_file_path, encoding = 'utf-8', index=False)   
-
-def progressBar(value, endvalue, bar_length=20):
-    percent = float(value) / endvalue
-    arrow = '-' * int(round(percent * bar_length)-1) + '>'
-    spaces = ' ' * (bar_length - len(arrow))
-    sys.stdout.write("\rPercent: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
-    sys.stdout.flush()
+    df.to_csv(out_file_path, encoding = encoding, index=False)   
 
 
 def remove_duplicate_activity(in_file_path, out_file_path, case_id_idx, activity_idx, timestamp_idx, encoding='utf-8'):
@@ -144,6 +145,78 @@ def insert_start_end_time(in_file_path, out_file_path, case_id_idx, activity_idx
         fw.writerow(prev_row)
 
 
+def fill_null_activity_(file_path, activity, fill_data, encoding='utf-8'):
+    '''
+    케이스당 null 엑티비티가 있을 경우, 특정 칼럼의 데이터를 복사해와 null에 넣어주는 함수
+    pandas를 이용
+
+    parameters
+    -------------------------------
+    file_path : string / header와 reader의 형태로 존재하는 raw log file형태 / raw log file 경로
+    activity : string / / activity 컬럼 명
+    fill_data : string /  / activity 컬럼에서 빈 row를 채워주기 위한 data column
+    encoding : stirng / / raw log file을 읽어드릴 때 encoding 방법
+    '''
+
+    df = pd.read_csv(file_path, encoding= encoding)
+    df[activity].fillna(df[fill_data], inplace=True)
+
+    df.to_csv(file_path, encoding = encoding, index=False)
+
+def case_modeling_(in_file_path, out_file_path, old_case_id, new_case_id, checking_data, timestamp, encoding = "utf-8"):
+    '''
+    다른 case이지만 log상에서 구별이 되지 않아 하나의 case로 취급될 경우, log를 이용하여 서로 다른 case로 구분되도록 모델링 하는 함수
+    pandas이용
+
+    parameters
+    -------------------------------
+    in_file_path : string / header와 reader의 형태로 존재하는 raw log file형태 / raw log file 경로
+    out_file_path : string / header와 reader의 형태로 존재하는 raw log file형태 / 새로 작성될 raw log file 경로
+    old_case_id : string /   / case id 컬럼 명
+    new_case_id : string /   / 새로 작성될 case id 컬럼 명
+    checking_data : string /    / case를 나누기 위한 log 데이터 컬럼 명
+    timestamp : string /    / timestamp 컬럼 명
+    encoding : stirng /    / raw log file을 읽어드릴 때 encoding 방법
+    '''
+    #data import and sort
+    date_parser = lambda x: pd.datetime.strptime(x, '%Y%m%d %H:%M:%S')
+    df = pd.read_csv(in_file_path, encoding= encoding, dtype = {old_case_id : str},parse_dates=[timestamp], date_parser=date_parser)
+    df.sort_values(by=[old_case_id, timestamp], inplace = True)
+    old_case_id_idx = df.columns.get_loc(old_case_id)
+    new_case_id_idx = df.columns.get_loc(new_case_id)
+    timestamp_idx = df.columns.get_loc(timestamp)
+
+
+    #temp data set
+    tmp_var_time = df.iloc[0, timestamp_idx]
+    tmp_var_id = df.iloc[0, old_case_id_idx]+ '_'+tmp_var_time.strftime('%Y%m%d %H:%M:%S')
+
+    #reference time set
+    dt1 = datetime.datetime(1988, 9, 16, 0, 0 ,0)
+    dt2 = datetime.datetime(1988, 9, 16, 6, 0, 0)
+    ref_time = dt2 - dt1
+
+    #find checking data
+    check_list = df[checking_data].str.match(r'(.*FxApp)|(.*InitApp)')
+
+    #new case insert
+    flag = 0
+    for k in check_list:
+        if k:
+            if df.iloc[flag, timestamp_idx] - tmp_var_time < ref_time:
+                df.iloc[flag, new_case_id_idx] = tmp_var_id
+            else:
+                tmp_var_time = df.iloc[flag, timestamp_idx]
+                tmp_var_id = df.iloc[flag, old_case_id_idx]+ '_'+tmp_var_time.strftime('%Y%m%d %H:%M:%S')
+                df.iloc[flag, new_case_id_idx] = tmp_var_id
+        else:
+            df.iloc[flag, new_case_id_idx] = tmp_var_id
+        flag += 1
+
+    #export
+    df.to_csv(out_file_path, encoding = encoding, index=False)
+
 if __name__ == '__main__':
-    remove_duplicate_activity("C:\\Users\\ko\\Desktop\\hynix python analyze\\data\\random_data10.csv", "C:\\Users\\ko\\Desktop\\hynix python analyze\\data\\sorted_out.csv",0,1,3,'utf-8')
-    insert_start_end_time("C:\\Users\\ko\\Desktop\\hynix python analyze\\data\\sorted_out.csv", "C:\\Users\\ko\\Desktop\\hynix python analyze\\data\\start_end_out.csv",0,1,3,'utf-8')
+    #remove_duplicate_activity("C:\\Users\\ko\\Desktop\\hynix analyze\\data\\random_data10.csv", "C:\\Users\\ko\\Desktop\\hynix analyze\\data\\sorted_out.csv",0,1,3,'utf-8')
+    #insert_start_end_time("C:\\Users\\ko\\Desktop\\hynix analyze\\data\\sorted_out.csv", "C:\\Users\\ko\\Desktop\\hynix analyze\\data\\start_end_out.csv",0,1,3,'utf-8')
+    #fill_null_activity_by_pandas("C:\\Users\\ko\\Desktop\\hynix analyze\\data\\random_data10.csv", "Activity", "Resource")
