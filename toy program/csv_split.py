c9dcacaee78c87a37a9b0e9d8a51c199cab624c7
chickchop <kohop8888@gmail.com>
2018-05-03 09:34:56 +0900

    Create csv_split.py
    대용량 csv 파일 분할 첫 작성

---
+++ toy program/csv_split.py
@@ -0,0 +1,33 @@
+def csv_split():
+    n_div_cnt = 200000
+
+    file_path = "D:\\"
+    file_name = "log_data"
+    file_exe = ".csv"
+
+    file_folder = "div_file\\"
+    
+    dir_name = file_path+file_folder
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
+
+    file_idx = 0
+    n_line_cnt = 0
+
+    with open(file_path + file_name+file_exe, "r", encoding='utf-8') as f:
+        g = open(dir_name + file_name+file_idx+file_exe,'w', encoding='utf-8')
+        while True:
+            line = f.readline()
+
+            if not line:
+                break
+
+            if n_line_cnt == n_div_cnt:
+                g.close()
+                file_idx += 1
+                n_line_cnt = 0
+                g = open(dir_name + file_name+file_idx+file_exe,'w', encoding='utf-8') 
+
+            n_line_cnt += 1
+            g.write(line)
+        g.close()
