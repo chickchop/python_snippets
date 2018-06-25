b3c4fe6abb24840a161ee8e1832db665e4004762
chickchop <kohop8888@gmail.com>
2018-06-25 16:54:42 +0900

    library 사용/비사용 분리

--- pz_pre_processing/sys_log_preprocessing.py
+++ pz_pre_processing/sys_log_preprocessing.py
@@ -8,7 +8,7 @@ import datetime
 from operator import itemgetter
 
 
-def remove_duplicate_activity_by_pandas(df, case_id, timestamp, activity):
+def remove_duplicate_activity_(df, case_id, timestamp, activity):
     """
     케이스당 연속되는 엑티비티를 중복으로 보고 삭제하는 함수. pandas를 이용
     parameters
@@ -36,44 +36,6 @@ def remove_duplicate_activity_by_pandas(df, case_id, timestamp, activity):
     df.drop(drop_list, inplace=True)
 
     return df
-
-
-def remove_duplicate_activity(in_file, out_file, case_id_idx, activity_idx, timestamp_idx, encoding='utf-8'):
-    """
-    케이스당 연속되는 엑티비티를 중복으로 보고 삭제하는 함수.pandas를 이용할 경우 낭비되는 메모리와 느린 속도 개선.
-    parameters
-    -------------------------------
-    in_file : string / header와 reader의 형태로 존재하는 raw log file형태 / raw log file 경로
-    out_file : string / header와 reader의 형태로 존재하는 raw log file형태 / 새로 작성될 raw log file 경로
-    case_id_idx : int /   / case id 열 위치
-    activity_idx : int /    / activity 열 위치
-    timestamp_idx : int /    / timestamp 열 위치
-    encoding : string /    / raw log file을 읽어드릴 때 encoding 방법
-    """
-    df = list()
-    rows = list()
-    with open(in_file, "r", encoding=encoding) as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        header = next(reader)
-        for row in reader:
-            rows.append(row)
-
-        rows.sort(key=itemgetter(case_id_idx, timestamp_idx))
-
-        df.append(header)
-        df.extend(rows)
-
-        fw = csv.writer(open(out_file, "w", newline='', encoding=encoding))
-
-        k = 0
-        while k < len(df):
-            if k <= 1 or \
-                    k == len(df) - 1 or \
-                    df[k][case_id_idx] != df[k - 1][case_id_idx] or \
-                    df[k][activity_idx] != df[k - 1][activity_idx] or \
-                    df[k][case_id_idx] != df[k + 1][case_id_idx]:
-                fw.writerow(df[k])
-            k = k + 1
 
 
 def insert_start_end_time(in_file, out_file, case_id_idx, activity_idx, timestamp_idx, encoding='utf-8'):
@@ -173,22 +135,23 @@ def case_modeling_(df, old_case_id, new_case_id, checking_col, timestamp, regex_
     old_case_id_idx = df.columns.get_loc(old_case_id)
     new_case_id_idx = df.columns.get_loc(new_case_id)
     timestamp_idx = df.columns.get_loc(timestamp)
-
+    print(1)
     # temp data set
     tmp_var_time = df.iloc[0, timestamp_idx]
     tmp_var_id = df.iloc[0, old_case_id_idx] + '_' + tmp_var_time.strftime('%Y%m%d %H:%M:%S')
-
+    print(2)
     # reference time set
     dt1 = datetime.datetime(1988, 9, 16, 0, 0, 0)
-    dt2 = datetime.datetime(1988, 9, 16, 6, 0, 0)
+    dt2 = datetime.datetime(1988, 9, 16, 8, 0, 0)
     ref_time = dt2 - dt1
-
+    print(3)
     # find checking data
     check_list = df[checking_col].str.match(regex_var)
 
     # new case insert
     flag = 0
     for k in check_list:
+        print("processing... %d" % flag)
         if k:
             if df.iloc[flag, timestamp_idx] - tmp_var_time > ref_time and \
                     df.iloc[flag, old_case_id_idx] == df.iloc[flag - 1, old_case_id_idx]:
@@ -204,7 +167,9 @@ def case_modeling_(df, old_case_id, new_case_id, checking_col, timestamp, regex_
         else:
             df.iloc[flag, new_case_id_idx] = tmp_var_id
         flag += 1
-
+        
+        
+    print(4)
     return df
 
 
@@ -231,28 +196,3 @@ def drop_certain_event_(df, checking_col, regex_var):
     df.drop(del_list, 0, inplace=True)
 
     return df
-
-# def big_log_transform(file_path, file_name, chunksize=10 ** 6, encoding='utf-8', case_id='CaseID',
-#                       timestamp='Timestamp', activity='Activity'):
-#     file_size = os.path.getsize(file_path + file_name)
-#     progress_size = 0
-#     for chunk in pd.read_csv(file_path + file_name, encoding=encoding, chunksize=chunksize):
-#         flag = 0
-#         progress_size = progress_size + chunksize
-#         progressBar(progress_size, file_size)
-#
-#         chunk = chunk.sort_values(by=[case_id, timestamp])
-#         k = 1
-#         while k < len(chunk):
-#             if chunk[case_id].iloc[k] == chunk[case_id].iloc[k - 1]:
-#                 if chunk[activity].iloc[k] == chunk[activity].iloc[k - 1]:
-#                     if chunk[activity].iloc[k] == chunk[activity].iloc[k + 1]:
-#                         pass
-#                     else:
-#                         chunk.drop(chunk.index[k], inplace=True)
-#                         k = k - 1
-#             k = k + 1
-#
-#         chunk.to_csv(file_path + "\\outdata" + str(flag) + file_name, encoding='utf-8', index=False)
-#         flag = flag + 1
-#     print("--- %s seconds ---" % (time.time() - start_time))
